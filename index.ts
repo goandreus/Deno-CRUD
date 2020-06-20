151a84e7627b5aedd95795fb775523dc4437d638
goandreus <andreshtml5@gmail.com>
2020-06-19 20:32:21 -0500

    create middleware

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('hellow word')
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+
+const router = new Router();
+const app = new Application();
+
+router.get('/', ({response}) => {
+    response.body = 'hello word'
+});
+
+app.use(router.routes());
+
+console.log('server running on port', 3000);
+
+await app.listen({port:3000});
